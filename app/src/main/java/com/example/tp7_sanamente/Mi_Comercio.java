b6ad4b9593c559575304e7e22e51de821d4e065f
Adriel Rasjido <adriel.rasjido@alumnos.frgp.utn.edu.ar>
2023-09-16 17:52:35 -0300

    Pantallas Adriel

---
+++ app/src/main/java/com/example/tp7_sanamente/Mi_Comercio.java
@@ -0,0 +1,14 @@
+package com.example.tp7_sanamente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mi_Comercio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mi_comercio);
+    }
+}
