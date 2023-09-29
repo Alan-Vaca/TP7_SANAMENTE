1fbfe325cf0c5f4c589398dfcda89095bd739b08
Adriel Rasjido <adriel.rasjido@alumnos.frgp.utn.edu.ar>
2023-09-29 20:06:34 -0300

    Todas las pantallas

---
+++ app/src/main/java/com/example/tp7_sanamente/MenuComercio.java
@@ -0,0 +1,14 @@
+package com.example.tp7_sanamente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuComercio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_comercio);
+    }
+}
---
+++ app/src/main/java/com/example/tp7_sanamente/ModificarProducto.java
@@ -0,0 +1,14 @@
+package com.example.tp7_sanamente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModificarProducto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificar_producto);
+    }
+}
