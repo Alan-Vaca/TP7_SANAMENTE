36dc8b4133c251aba67d1ddf4b49c2e16483421f
Alan-Vaca <alan.vaca@alumnos.frgp.utn.edu.ar>
2023-10-05 20:42:43 -0300

    Punto de menu catalogo parte 2

--- app/src/main/java/com/example/tp7_sanamente/Metodo_De_Pago.java
+++ app/src/main/java/com/example/tp7_sanamente/Metodo_De_Pago.java
@@ -2,7 +2,9 @@ package com.example.tp7_sanamente;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Metodo_De_Pago extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class Metodo_De_Pago extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metodo_de_pago);
+    }
+
+
+    public void MetodoDePagoFinalizarCompra(View view) {
+        Intent metodoDePagoFinalizarCompra = new Intent(this, MiHistorial.class);
+        startActivity(metodoDePagoFinalizarCompra);
+    }
+
+    public void MetodoDePagoCancelarCompra(View view) {
+        Intent metodoDePagoCancelarCompra = new Intent(this, MiCarritoCompras.class);
+        startActivity(metodoDePagoCancelarCompra);
     }
 }
--- app/src/main/java/com/example/tp7_sanamente/MiCarritoCompras.java
+++ app/src/main/java/com/example/tp7_sanamente/MiCarritoCompras.java
@@ -2,7 +2,9 @@ package com.example.tp7_sanamente;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MiCarritoCompras extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MiCarritoCompras extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mi_carrito_compras);
+    }
+
+    public void MiCarritoDeComprasSeguirComprando(View view) {
+        Intent miCarritoDeComprasSeguirComprando = new Intent(this, Mis_Productos.class);
+        startActivity(miCarritoDeComprasSeguirComprando);
+    }
+
+    public void MiCarritoDeComprasComprar(View view) {
+        Intent miCarritoDeComprasComprar = new Intent(this, Metodo_De_Pago.class);
+        startActivity(miCarritoDeComprasComprar);
     }
 }
