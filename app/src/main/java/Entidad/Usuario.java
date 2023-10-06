ba6c4edd477a312d3e953f158c732edce5b758f9
Adriel Rasjido <adriel.rasjido@alumnos.frgp.utn.edu.ar>
2023-10-05 22:16:06 -0300

    Agregar Cliente

--- app/src/main/java/Entidad/Usuario.java
+++ app/src/main/java/Entidad/Usuario.java
@@ -1,6 +1,9 @@
 package Entidad;
 
-public class Usuario {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Usuario implements Parcelable{
     int idUsuario;
     String nombreUsuario;
     String nombre;
@@ -98,5 +101,50 @@ public class Usuario {
                 ", Direccion='" + Direccion + '\'' +
                 ", Estado=" + Estado +
                 '}';
+    }
+
+
+
+
+    protected Usuario(Parcel in) {
+        idUsuario = in.readInt();
+        nombreUsuario = in.readString();
+        nombre = in.readString();
+        Apellido = in.readString();
+        Contraseña = in.readString();
+        DNI = in.readInt();
+        Direccion = in.readString();
+        Estado = in.readByte() != 0;
+        Cliente = in.readByte() != 0;
+    }
+
+    public static final Creator<Usuario> CREATOR = new Creator<Usuario>() {
+        @Override
+        public Usuario createFromParcel(Parcel in) {
+            return new Usuario(in);
+        }
+
+        @Override
+        public Usuario[] newArray(int size) {
+            return new Usuario[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(idUsuario);
+        dest.writeString(nombreUsuario);
+        dest.writeString(nombre);
+        dest.writeString(Apellido);
+        dest.writeString(Contraseña);
+        dest.writeInt(DNI);
+        dest.writeString(Direccion);
+        dest.writeByte((byte) (Estado ? 1 : 0));
+        dest.writeByte((byte) (Cliente ? 1 : 0));
     }
 }
--- app/src/main/java/com/example/tp7_sanamente/Registrar_Cliente.java
+++ app/src/main/java/com/example/tp7_sanamente/Registrar_Cliente.java
@@ -1,17 +1,46 @@
 package com.example.tp7_sanamente;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.PopUpToBuilder;
+
+import BaseDeDatos.Conexion;
+import Entidad.Cliente;
+import Entidad.Restriccion;
+import Entidad.Usuario;
 
 public class Registrar_Cliente extends AppCompatActivity {
+
+
+    TextView nombre,apellido,dni,alergias, direccion;
+    CheckBox hipertenso,diabetico,celiaco;
+
+    Usuario user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_cliente);
+
+        Usuario usuarioRecibido = getIntent().getParcelableExtra("usuarioInsert");
+        user = usuarioRecibido;
+
+        nombre = (EditText)findViewById(R.id.r_cli_nombre);
+        apellido = (EditText)findViewById(R.id.r_cli_apellido);
+        dni = (EditText)findViewById(R.id.r_cli_dni);
+        alergias = (EditText)findViewById(R.id.r_cli_alergias);
+        direccion = (EditText)findViewById(R.id.r_cli_direccion);
+        hipertenso = (CheckBox)findViewById(R.id.r_cb_hipertenso);
+        diabetico = (CheckBox)findViewById(R.id.r_cb_diabetico);
+        celiaco = (CheckBox)findViewById(R.id.r_cb_celiaco);
     }
 
     public void VolverRegistro(View view) {
@@ -20,7 +49,58 @@ public class Registrar_Cliente extends AppCompatActivity {
     }
 
     public void IngresarCliente(View view) {
-        Intent IngresarCliente = new Intent(this, MainActivity.class);
-        startActivity(IngresarCliente);
+
+        Restriccion restriccion = new Restriccion();
+        restriccion.setAlergico(alergias.getText().toString());
+        restriccion.setHipertenso(hipertenso.isChecked());
+        restriccion.setDiabetico(hipertenso.isChecked());
+        restriccion.setCeliaco(celiaco.isChecked());
+
+        Cliente cliente = new Cliente();
+        user.setNombre(nombre.getText().toString());
+        user.setApellido(apellido.getText().toString());
+        user.setDNI(Integer.parseInt(dni.getText().toString()));
+        user.setDireccion(direccion.getText().toString());
+        cliente.setUsuarioAsociado(user);
+        restriccion.setClienteAsociado(cliente);
+
+        if(validarCliente(restriccion)) {
+            new registrarCliente().execute(restriccion);
+        }
+    }
+
+    public boolean validarCliente(Restriccion res){
+        return true;
+    }
+
+
+    private class registrarCliente extends AsyncTask<Restriccion, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Restriccion... res) {
+            Conexion con = new Conexion();
+            boolean exito = false;
+            try {
+                exito = con.RegistrarUsuarioCliente(res[0]);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+            return exito;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean exito) {
+
+            if(exito) {
+                Toast.makeText(Registrar_Cliente.this, "CLIENTE AGREGADO CON EXITO", Toast.LENGTH_LONG).show();
+
+                Intent IngresarCliente = new Intent(Registrar_Cliente.this, MainActivity.class);
+                startActivity(IngresarCliente);
+
+            }else {
+                Toast.makeText(Registrar_Cliente.this, "HUBO UN ERROR", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
